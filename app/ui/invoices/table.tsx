09da26f59009cc4d01a98a5cffb4528de84c22d3
Saikalyan Akunuri <saikalyan.akunuri@gmail.com>
2024-07-12 23:28:25 +0530

    create and update invoice functionality completed

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -33,7 +33,7 @@ export default async function InvoicesTable({
                         height={28}
                         alt={`${invoice.name}'s profile picture`}
                       />
-                      <p>{invoice.name}</p>
+                      <p>{invoice.name}</p>                      
                     </div>
                     <p className="text-sm text-gray-500">{invoice.email}</p>
                   </div>
@@ -106,6 +106,7 @@ export default async function InvoicesTable({
                   </td>
                   <td className="whitespace-nowrap px-3 py-3">
                     <InvoiceStatus status={invoice.status} />
+                    
                   </td>
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex justify-end gap-3">
