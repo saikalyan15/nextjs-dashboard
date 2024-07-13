0459322b878acb0512e64b6c99db3611c64c678d
Saikalyan Akunuri <saikalyan.akunuri@gmail.com>
2024-07-13 23:58:22 +0530

    fixed home page

---
+++ app/search.tsx
@@ -0,0 +1,19 @@
+'use client'
+ 
+import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
+ 
+function Search() {
+  const searchParams = useSearchParams()
+ 
+  return <input placeholder="Search..." />
+}
+ 
+export function Searchbar() {
+  return (
+    // You could have a loading skeleton as the `fallback` too
+    <Suspense>
+      <Search />
+    </Suspense>
+  )
+}
